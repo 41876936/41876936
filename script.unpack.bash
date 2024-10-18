9030c24d79a008c6440fb1f17b6ca2f30cdcf2ec
41876936 <joseph.vautour@gmail.com>
2024-10-18 04:55:14 -0400

    IT output and correct frame of reference

--- script.unpack.bash
+++ script.unpack.bash
@@ -2,3 +2,7 @@ tar -xvf script.html.xz.tar
 cd script.html
 cd script.script.bash
 . render_png.bash
+cd ..
+echo the script may be ready to view presently in an html viewer.
+echo your current working directory is:
+pwd
